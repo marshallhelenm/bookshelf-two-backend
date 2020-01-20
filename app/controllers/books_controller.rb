3614ec18a4c546c1f03c5fdc66f082dbf20ee6fc
marshallhelenm <marshallhelenm@gmail.com>
2020-01-19 19:27:41 -0800

    looks like we hadnt even been taught crud yet when we made the original! gonna be a lot of adapting to do.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,36 @@
 class BooksController < ApplicationController
-    def remove_book(shelf, active_user)
-        if shelf.books.empty?
-            puts "\nThis shelf is empty!"
-        else
-            puts `clear`
-            puts "Which book should we remove? Please enter a number:"
-            shelf.view_shelf_contents
-            book_index = STDIN.gets.chomp.to_i - 1
-            book = shelf.books[book_index] #grab that book instance off their shelf
-            shelf.remove_book_from_shelf(book) #calling the shelf instance method called remove_book_from_shelf
-            active_user = update_user_variable(active_user)
-            puts "\nYou have successfully removed #{book.title} from #{shelf.name}."
-            puts "\nShelf contents: "
-            shelf.view_shelf_contents
-        end
-        active_user
+    def index
+        
     end
     
-    def add_book(shelf)#takes a shelf instance, and calls on the shelf instance method, add_book
-        book = Book.find_book #will return a book instance if the user was successful, or nil if they gave up
-        shelf.add_book_to_shelf(book) if book
-        STDIN.gets.chomp
+    def create
+        book = Book.new(title: book_params[title], description: book_params[description])
+        book.save
+    end
+    
+    def edit
+    
+    end
+    
+    def update
+    
+    end
+    
+    def show
+    
+    end
+    
+    def destroy
+    
     end
 
+    def add_to_shelf(shelf)
+        shelf.books << self
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title, :description, :id)
+    end
 end
